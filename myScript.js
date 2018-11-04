a1ccab0778aa901f00de722680fc8e4de049b566
ninomiyaonion <xumanluu@gmail.com>
2018-11-04 12:19:07 +0100

    pdf added

--- myScript.js
+++ myScript.js
@@ -3,3 +3,5 @@ var imgObj = document.getElementById("avatar");
 var Flag=(imgObj.getAttribute("src",2)=="https://i.imgur.com/wKoyIPY.png")
 imgObj.src=Flag?"https://i.imgur.com/0jkNLIA.png":"https://i.imgur.com/wKoyIPY.png";
 }
+
+$('a.media').media({width:500, height:400});
