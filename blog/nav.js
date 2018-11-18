60ac4ac4f0cc371e3d068203e7f8142b01b87692
ninomiyaonion <xumanluu@gmail.com>
2018-11-18 21:37:02 +0100

    blognav update

--- blog/nav.js
+++ blog/nav.js
@@ -1,9 +1,22 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
+//window.onscroll = function() {scrollFunction()};
+
+//function scrollFunction() {
+//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//    document.getElementById("navbar").style.top = "0";
+//  } else {
+//    document.getElementById("navbar").style.top = "-50px";
+//  }
+//}
+var bodyClass = document.body.classList,
+    lastScrollY = 0;
+window.addEventListener('scroll', function(){
+  var st = this.scrollY;
+  // 　是否向上滑动页面
+  if( st < lastScrollY) {
+    bodyClass.remove('hideUp');
+  }else{
+    bodyClass.add('hideUp');
   }
-}
+  lastScrollY = st;
+});
